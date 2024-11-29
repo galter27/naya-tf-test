20fd01b064760534b4ad38e015582bc0b58c6d61
gternovs <gternovs@akamai.com>
2024-11-29 11:45:50 +0200

    Changed structure

---
+++ terraform/variables.tf
@@ -0,0 +1,31 @@
+################### VPC Varibles ###################
+
+variable "vpc_name" {
+  description = "The name of the VPC"
+  type        = string
+  default     = "my-vpc"  
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
+
+variable "private_subnets" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24"]
+}
+
+variable "database_subnets" {
+  description = "CIDR blocks for database subnets"
+  type        = list(string)
+  default     = ["10.0.201.0/24", "10.0.202.0/24"]
+}
